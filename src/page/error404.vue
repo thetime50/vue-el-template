042aa08f32b77e28ba78a899ee5f3332bde9b3bf
luoxin <A33@mlf.com>
2021-01-12 18:06:11 +0800

    component-error404

--- src/page/error404.vue
+++ src/page/error404.vue
@@ -1,8 +1,10 @@
 <template>
-<div class="component-error404 vertical-center">
-    error404<br>
-    找不到路径{{$route.fullPath}}<br>
-    <el-button type="danger" plain @click="$router.go(-1)">返回</el-button>
+<div class="component-error404">
+    <div class="block vertical-center">
+        error404<br>
+        找不到路径{{$route.fullPath}}<br>
+        <el-button type="danger" plain @click="$router.go(-1)">返回</el-button>
+    </div>
 </div>
 </template>
 
@@ -20,14 +22,16 @@ export default {
 
 <style rel="stylesheet/scss" lang="scss" scoped>
 .component-error404{
-    margin: auto;
-    padding: 20px;
-    color:#f55;
-    background-color: #fee;
-    border: 1px solid #f88;
-    border-radius: 15px;
-    width: 400px;
-    text-align: center;
-    top:40%;
+    .block{
+        margin: auto;
+        padding: 20px;
+        color:#f55;
+        background-color: #fee;
+        border: 1px solid #f88;
+        border-radius: 15px;
+        width: 400px;
+        text-align: center;
+        top:40%;
+    }
 }
 </style>
